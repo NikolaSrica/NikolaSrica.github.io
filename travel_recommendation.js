3d0c986a2c528c389c319ad273d51e46e7c45f69
Your Name <you@example.com>
2025-03-08 05:14:32 -0500

    message3

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,6 +1,8 @@
 // travel_recommendation.js
 
 
+const btnSearch = document.getElementById('btnSearch');
+
 function searchCondition() {
 const input = document.getElementById('conditionInput').value.toLowerCase();
 const cardContainer = document.getElementById('card-container');
@@ -8,10 +10,11 @@ cardContainer.innerHTML = '';
 fetch('travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
-      
-        const country = data.countries.find(item => item.name.toLowerCase() === input);
 
-    if(country){
+        const country = data.countries.find(item => item.name.toLowerCase() === input);
+  
+
+        if(country){
         country.cities.forEach(city => {
             const card = document.createElement('div');
             card.className = 'card';
@@ -21,6 +24,38 @@ fetch('travel_recommendation_api.json')
                 <div class="card-content">
                     <h3>${city.name}</h3>
                     <p>${city.description}</p>
+                </div>
+            `;
+
+            cardContainer.appendChild(card);
+        });
+    }
+    if(input === 'temples'){
+        data.temples.forEach(temple => {
+            const card = document.createElement('div');
+            card.className = 'card';
+
+            card.innerHTML = `
+                <img src="${temple.imageUrl}" alt="${temple.name}">
+                <div class="card-content">
+                    <h3>${temple.name}</h3>
+                    <p>${temple.description}</p>
+                </div>
+            `;
+
+            cardContainer.appendChild(card);
+        });
+    }
+    if(input === 'beaches'){
+        data.beaches.forEach(beach => {
+            const card = document.createElement('div');
+            card.className = 'card';
+
+            card.innerHTML = `
+                <img src="${beach.imageUrl}" alt="${beach.name}">
+                <div class="card-content">
+                    <h3>${beach.name}</h3>
+                    <p>${beach.description}</p>
                 </div>
             `;
 
